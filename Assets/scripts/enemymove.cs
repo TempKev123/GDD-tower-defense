b8a436410a341a6fbb1903696359578ac1a0df4f
Tempkev123 <kevindityam@gmail.com>
2025-09-13 19:22:31 +0700

    enemy helth

--- Assets/scripts/enemymove.cs
+++ Assets/scripts/enemymove.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class enemymove : MonoBehaviour
 {
+    public int hp;
     public float speed;
     // Start is called before the first frame update
     void Start()
@@ -15,5 +16,13 @@ public class enemymove : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        if (hp <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void takeDamage(int damage)
+    {
+        hp -= damage;
     }
 }
