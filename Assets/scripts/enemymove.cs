bf02b6e8b35dcabaffac98c6ae7d8cda3e05ac99
Pachara32 <153341039+Pachara32@users.noreply.github.com>
2025-09-20 15:30:16 +0700

    add ui again

--- Assets/scripts/enemymove.cs
+++ Assets/scripts/enemymove.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class enemymove : MonoBehaviour
 {
-    //public int hp;
+    public int hp;
     public float speed;
     // Start is called before the first frame update
     void Start()
@@ -16,15 +16,13 @@ public class enemymove : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
-        //if (hp <= 0)
-        //{
-        //    Destroy(gameObject);
-        //}
+        if (hp <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
-    /*
     void takeDamage(int damage)
     {
         hp -= damage;
     }
-    */
 }
